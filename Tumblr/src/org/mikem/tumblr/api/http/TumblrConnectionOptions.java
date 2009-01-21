fdd27fe72c28251d4b3d089364963ee35c5a1da7
mike.mclean.1 <mike.mclean.1@7ba5eee4-ce44-11dd-86c2-0f8696b7b6f9>
2009-01-21 02:20:35 +0000

    Email and Password aren't needed
    git-svn-id: http://tumblrj.googlecode.com/svn/trunk@22 7ba5eee4-ce44-11dd-86c2-0f8696b7b6f9

--- Tumblr/src/org/mikem/tumblr/api/http/TumblrConnectionOptions.java
+++ Tumblr/src/org/mikem/tumblr/api/http/TumblrConnectionOptions.java
@@ -5,8 +5,6 @@ import org.apache.commons.httpclient.params.HttpClientParams;
 
 public class TumblrConnectionOptions {
 	private String name;
-	private String email;
-	private String password;
 	private HttpClientParams httpClientParams;
 	
 	public HttpClientParams getHttpClientParams() {
@@ -14,18 +12,6 @@ public class TumblrConnectionOptions {
 	}
 	public void setHttpClientParams(HttpClientParams httpClientParams) {
 		this.httpClientParams = httpClientParams;
-	}
-	public String getEmail() {
-		return email;
-	}
-	public void setEmail(String email) {
-		this.email = email;
-	}
-	public String getPassword() {
-		return password;
-	}
-	public void setPassword(String password) {
-		this.password = password;
 	}
 	public String getName() {
 		return name;
